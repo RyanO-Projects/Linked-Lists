034d73c79ba63cdbe47cee4753ffe2ae04eb7f73
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-10 16:20:53 -0700

    Changed Variable Name
    ListNode pointer object from next to nextLtbC to avoid confusion.

--- NumberList.cpp
+++ NumberList.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // Copy Constructor
 NumberList::NumberList(const NumberList &obj){
     ListNode *current;
-    ListNode *next;
+    ListNode *nextLtbC; // nextLtbC == "next Link to be Copied"
 
     if(obj.head == nullptr)
         head = nullptr;
@@ -15,32 +15,32 @@ NumberList::NumberList(const NumberList &obj){
         head->next = nullptr;
         head->value = obj.head->value;
         current = head;
-        next = obj.head->next;
+        nextLtbC = obj.head->next;
     }
 
-    while(next){
-        current->next = new ListNode;   // Create new node for next pointer
-        current = current->next;        // Move current pointer to newly created ListNode
-        current->next = nullptr;        // Initialize next pointer
-        current->value = next->value;   // Copy next value to current value
-        next = next->next;              // Make next point to next's next pointer.
+    while(nextLtbC){
+        current->next = new ListNode;       // Create new node for next pointer
+        current = current->next;            // Move current pointer to newly created ListNode
+        current->next = nullptr;            // Initialize next pointer
+        current->value = nextLtbC->value;   // Copy next value to current value
+        nextLtbC = nextLtbC->next;          // Make next point to nextLtbC's next pointer.
     }
 }
 
 void NumberList::appendNode(double num){
-  // make a new node
-  ListNode* newNode = new ListNode;
-  // set fields
-  newNode->value = num;
-  newNode->next = nullptr; 
+    // make a new node
+    ListNode* newNode = new ListNode;
+    // set fields
+    newNode->value = num;
+    newNode->next = nullptr; 
 
-  if (!head)  // head is the nullpointer, empty list
-    head = newNode;
-  else {  // list is not empty, 
-    ListNode* nodePtr = head;  
-    while (nodePtr->next)
-      nodePtr = nodePtr->next;
-    nodePtr->next = newNode;    
+    if (!head)  // head is the nullpointer, empty list
+        head = newNode;
+    else {  // list is not empty, 
+        ListNode* nodePtr = head;  
+        while (nodePtr->next)
+            nodePtr = nodePtr->next;
+        nodePtr->next = newNode;    
     }
   }
 

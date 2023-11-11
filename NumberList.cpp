f376ae78d5d81fdee7a4d6b889012d20c11812fa
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-10 17:17:21 -0700

    displayList() and ~Destructor()
    Added function to display values in nodes of linked list. Traverses list and displays value in each node.
    
    Added destructor. Traverses list and deletes each node.
    
    Also started reverseList() function.

--- NumberList.cpp
+++ NumberList.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // Copy Constructor
 NumberList::NumberList(const NumberList &obj){
     ListNode *current;
-    ListNode *nextLtbC; // nextLtbC == "next Link to be Copied"
+    ListNode *nextNtbC; // nextLtbC == "next Node to be Copied"
 
     if(obj.head == nullptr)
         head = nullptr;
@@ -15,36 +15,70 @@ NumberList::NumberList(const NumberList &obj){
         head->next = nullptr;
         head->value = obj.head->value;
         current = head;
-        nextLtbC = obj.head->next;
+        nextNtbC = obj.head->next;
     }
 
-    while(nextLtbC){
+    while(nextNtbC){
         current->next = new ListNode;       // Create new node for next pointer
         current = current->next;            // Move current pointer to newly created ListNode
         current->next = nullptr;            // Initialize next pointer
-        current->value = nextLtbC->value;   // Copy next value to current value
-        nextLtbC = nextLtbC->next;          // Make next point to nextLtbC's next pointer.
+        current->value = nextNtbC->value;   // Copy next value to current value
+        nextNtbC = nextNtbC->next;          // Make next point to nextLtbC's next pointer.
     }
 }
 
+// Append node
 void NumberList::appendNode(double num){
-    // make a new node
     ListNode* newNode = new ListNode;
-    // set fields
+
     newNode->value = num;
     newNode->next = nullptr; 
 
-    if (!head)  // head is the nullpointer, empty list
+    if (!head)  // head == nullptr | List is empty
         head = newNode;
+
     else {  // list is not empty, 
         ListNode* nodePtr = head;  
         while (nodePtr->next)
             nodePtr = nodePtr->next;
         nodePtr->next = newNode;    
     }
-  }
+}
 
-  void NumberList::displayList() const {
+// Display contents of linked list | List Print
+void NumberList::displayList() const {
+    // To travel list
+    ListNode *nodePtr;
 
-  }
-  
+    // To point to haed of list
+    nodePtr = head;
+    while(nodePtr){     // While there is a node, display value
+        cout << nodePtr->value << endl;
+        nodePtr = nodePtr->next;
+    }
+}
+
+// List Reverse
+void NumberList::reverseList(){
+}
+
+// Destructor
+NumberList::~NumberList()
+{
+   ListNode *current;   // To traverse the list
+   ListNode *nextNtbD;  // To point to the next node
+
+   current = head;
+
+   while (current != nullptr)
+   {
+      // Save a pointer to the next node.
+      nextNtbD = current->next;
+
+      // Delete the current node.
+      delete current;
+
+      // Position nodePtr at the next node.
+      current = nextNtbD;
+   }
+}  
